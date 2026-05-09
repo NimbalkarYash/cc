2feed8ff03b7a4bbc19b406bc04c0a497d1a6ad3
yash nimbalkar <163175878+NimbalkarYash@users.noreply.github.com>
2026-05-09 16:31:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    alert("hi mere dost");
+
+    const age = 30;
+    const name = "yash";
+    const isMale = true;
+
+    console.log(age);
+    console.log(name);
+    console.log(isMale);
+
+    
